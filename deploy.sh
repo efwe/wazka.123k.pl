5a573eefa642a5f07a2588b5758fec3753872961
Frank Waldheim <fw@123k.org>
2019-02-01 22:10:33 +0100

    also works when nothing to be deleted

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git diff --quiet || (echo 'please commit' && exit 1)
-ssh fw@pluskwa.123k.org 'rm /usr/local/www/123k.work/*'
+ssh fw@pluskwa.123k.org 'rm -f /usr/local/www/123k.work/*'
 scp -P1023 ./dist/* fw@pluskwa.123k.org:/usr/local/www/123k.work
